cf619e0aa20791a914b9f607ce5c80196f872228
Michael <micoraafat96@gmail.com>
2018-12-26 01:41:30 +0200

    img_measures.py added

--- img_measures.py
+++ img_measures.py
@@ -19,7 +19,8 @@ def get_measures(gt_img, kimg):
         entropy = 0;
         for j in range(len(cont_mat)):
             percent_j = cont_mat[j][i] / np.sum(cont_mat[:,i]);
-            entropy -= percent_j * math.log10(percent_j);
+            if (percent_j != 0.0):
+                entropy -= percent_j * math.log(percent_j);
         entropy *= n_i / n;
         cond_entropy += entropy;
     f_measure /= len(cont_mat[0]);
